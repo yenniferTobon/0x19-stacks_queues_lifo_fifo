0f1c521f754eb05ccaedef4fda10f766941d4aff
yenniferTobon <ymtobony@unal.edu.co>
2019-09-25 01:20:46 +0000

    _Main.c

--- _Main.c
+++ _Main.c
@@ -2,6 +2,7 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <string.h>
 /**
  * _Main - function that
  * @argc: nothing
@@ -14,6 +15,7 @@ int main(int argc, char *argv[])
 	FILE *filedesc;
 	char *line_buf = NULL;
 	size_t sizeBuffer = 1024;
+	char *arguments;
 	int size = 0;
 
 	if (argc > 2 || argc == 1)
@@ -23,7 +25,15 @@ int main(int argc, char *argv[])
 	}
 
 	filedesc = fopen(argv[1], "r");
-	size = getline(&line_buf, &sizeBuffer, filedesc);
+	while (getline(&line_buf, &sizeBuffer, filedesc) != EOF)
+	{
+		arguments = strtok(line_buf, " \r\n\t");
+		while (arguments != NULL)
+		{
+			printf("%s\n", arguments);
+			arguments = strtok(NULL, " \r\n\t");
+		}
+	}
 	printf("%d\n", size);
 	/**printf("%s\n", argv[1]);**/
 	return (0);
